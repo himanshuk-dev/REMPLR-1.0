c22301fe63e462e27171c007ad3f0dbb7575ad7e
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-04-10 16:15:06 -0700

    adding recipe search by nutrients / ingredients

--- static/app.js
+++ static/app.js
@@ -1,8 +1,17 @@
 const base_url = "https://api.spoonacular.com";
 let apiKey = "ac0e17f073af4388a91d75452dfa1051";
 
-// Search bar
-function search() {
-  const searchInput = document.getElementById("searchInput").value;
-  const res = `${base_url}/food/ingredients/search?query=${searchInput}&apiKey=${apiKey}`;
-}
+/** Search for Recipes on homepage **/
+// Show nutrients form when "Nutrients" option is selected
+let searchCriteria = document.getElementById("search_criteria");
+let nutrientsForm = document.getElementById("nutrients_form");
+let searchForm = document.getElementById("search_recipe_form");
+
+searchCriteria.addEventListener("change", function () {
+  if (searchCriteria.value === "nutrients") {
+    nutrientsForm.style.display = "block";
+    searchForm.style.display = "none";
+  } else {
+    nutrientsForm.style.display = "none";
+  }
+});
