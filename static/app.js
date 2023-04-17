13c7eaaa228793e3393630f117e53c58247fbd62
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-04-17 15:25:40 -0700

    fixing meal planner start button

--- static/app.js
+++ static/app.js
@@ -1,15 +1,6 @@
 const base_url = "https://api.spoonacular.com/";
 let apiKey = "ac0e17f073af4388a91d75452dfa1051";
 
-/** Nav Bar  **/
-const menuIcon = document.querySelector(".menu-icon");
-const menu = document.querySelector(".menu");
-
-menuIcon.addEventListener("click", () => {
-  menuIcon.classList.toggle("open");
-  menu.classList.toggle("open");
-});
-
 /** Search for Recipes on homepage **/
 // Show nutrients form when "Nutrients" option is selected
 let searchCriteria = document.getElementById("search_criteria");
