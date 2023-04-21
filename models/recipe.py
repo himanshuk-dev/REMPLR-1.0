55f4bd4370691dd8b71b3dbfd2bbdc35e8425459
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-04-21 11:17:55 -0700

    adding functionality to save recipes in meal_plans

--- models/recipe.py
+++ models/recipe.py
@@ -8,13 +8,14 @@ class Recipe(db.Model):
     __tablename__ = 'recipes'
     
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    instructions = db.Column(db.Text, nullable=True)
-    ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredients.id', ondelete="CASCADE"))
+    name = db.Column(db.String, nullable=False)
+    image = db.Column(db.String, nullable = True)
+    serving = db.Column(db.Number, nullable = True)
+    meal_plan_id = db.Column(db.Integer, db.ForeignKey('meal_plans.id', ondelete="CASCADE"), nullable = True)
     user_id = db.Column(db.Integer, db.ForeignKey('nutritionists.id', ondelete="CASCADE"))
 
-    # Define one-to-many relationship between ingredients and recipes
-    ingredient = db.relationship('Ingredient', backref='recipes')
+    # Define one-to-many relationship between recipes and meal_plans
+    meal_plan = db.relationship('meal_plans', backref='recipes')
 
     def __repr__(self):
         return f'<Recipe {self.name}>'
