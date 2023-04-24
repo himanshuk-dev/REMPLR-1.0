857e63e39e8e6e91835a8f8f96bdbe1fc9345d93
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-04-24 16:01:44 -0700

    setting up environment for heroku

--- app.py
+++ app.py
@@ -16,7 +16,11 @@ app = Flask(__name__)
 app.app_context().push() 
 
 # Set app configuration using sqlAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///REMPLR'
+try:
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL").replace("://", "ql://", 1)
+except AttributeError:
+    # this is used locally and used to run tests
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///REMPLR'
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "lounge-BARBICAN-3158!"
